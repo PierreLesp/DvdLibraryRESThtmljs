6d228ca012d687945441409a7a6bc4bdbd044b7e
sol.han.dev <sol.han.dev@gmail.com>
2021-09-23 15:40:26 -0400

    Edit DVD Iteration 1.

--- DVDLibrary/js/home.js
+++ DVDLibrary/js/home.js
@@ -1,3 +1,87 @@
 $('document').ready(function() {
     alert('ready pol');
+    updateDVD();
 });
+
+function updateDVD(DVDId) {
+    $('#updateButton').click(function(event) {
+        /*var haveValidationErrors = checkAndDisplayValidationErrors($('#editForm').find('input'));
+        
+        if(haveValidationErrors) {
+            return false;
+        }*/
+        $.ajax({
+            type: 'PUT',
+            url: 'http://DVDlist.us-east-1.elasticbeanstalk.com/dvd/' + $('#editDVDId').val(),
+            data: JSON.stringify({
+                Id: $('#editDVDId').val(),
+                title: $('#editTitle').val(),
+                releaseYear: $('#editReleaseYear').val(),
+                director: $('#editDirector').val(),
+                rating: $('#editRating').val(),
+                notes: $('#editNotes').val()
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            'dataType': 'json',
+            'success': function() {
+                /*$('#errorMessage').empty();
+                hideEditForm();
+                loadDVDs();*/
+            },
+            'error': function() {
+                /*$('#errorMessages')
+                .append($('<li>')
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service. Please try again later.'));*/
+            }
+        })
+    })
+}
+
+function showEditForm(DVDId) {
+    //$('#errorMessages').empty();
+    
+    $.ajax({
+        type: 'GET',
+        url: 'http://DVDlist.us-east-1.elasticbeanstalk.com/dvd/' + DVDId,
+        success: function(data, status) {
+            $('#editTitle').val('');
+            $('#editReleaseYear').val('');
+            $('#editDirector').val('');
+            $('#editRating').val('');
+            $('#editNotes').val('');
+            
+            $('#editTitle').val(data.title);
+            $('#editReleaseYear').val(data.releaseYear);
+            $('#editDirector').val(data.director);
+            $('#editRating').val(data.rating);
+            $('#editNotes').val(data.notes);
+            
+        },
+        error: function() {
+            $('#errorMessages')
+            .append($('<li>')
+            .attr({class: 'list-group-item list-group-item-danger'})
+            .text('Error calling web service. Please try again later.')); 
+        }
+    })
+    
+    $('#contactTableDiv').hide();
+    $('#editFormDiv').show();
+}
+
+function hideEditForm() {
+    //$('#errorMessages').empty();
+    
+    $('#editTitle').val('');
+    $('#editReleaseYear').val('');
+    $('#editDirector').val('');
+    $('#editRating').val('');
+    $('#editNotes').val('');
+
+    //$('#DVDTableDiv').show();
+    $('#editFormDiv').hide();
+}
